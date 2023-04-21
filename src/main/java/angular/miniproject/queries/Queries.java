60a7c5479173153df849ac923cd09098b6e4ace9
Ironyrus <vans_iwan@yahoo.com>
2023-04-21 17:44:16 +0800

    GitHub Actions

--- src/main/java/angular/miniproject/queries/Queries.java
+++
@@ -1,7 +0,0 @@
-package angular.miniproject.queries;
-
-public class Queries {
-    public static String SQL_SIGNUP = "insert into logindata (email, password) values (?, ?)";
-
-    public static String SQL_CHECK_USERLOGIN = "select * from logindata where email = ?";
-}
